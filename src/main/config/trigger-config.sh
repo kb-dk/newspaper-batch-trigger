df12b1d3e6339214ab1677165f79a18626b24a81
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2014-11-12 14:15:48 +0100

    Update configuration to match anonymized version of production config

--- src/main/config/trigger-config.sh
+++ src/main/config/trigger-config.sh
@@ -6,22 +6,22 @@
 #
 
 # Where the received newspaper batches are placed
-path_to_dir_of_batches='/batches'
+path_to_dir_of_batches={avis.folder}
 
 # Fedora location
-url_to_doms='${fedora.server}'
+url_to_doms=http://{doms.host}:{doms.port}/fedora
 
 # Username for calls to DOMS
-doms_username='${fedora.admin.username}'
+doms_username={doms.username}
 
 # Password for calls to DOMS
-doms_password='${fedora.admin.password}'
+doms_password={doms.password}
 
 # Location of PID generator
-url_to_pid_gen='${pidgenerator.location}'
+url_to_pid_gen=http://{doms.host}:{doms.port}/pidgenerator-service
 
 # Name of the trigger-script
-trigger_name='register-batch-trigger'
+trigger_name=register-batch-trigger
 
 # Location of var folder for storing processed batches
-donedir="$HOME/var/batches-done"
+donedir=$HOME/var/batches-done
