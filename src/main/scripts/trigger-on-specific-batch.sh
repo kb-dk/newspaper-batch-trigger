a856e0c15f23717de036d30f632a3ad2950f8c58
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2014-12-16 09:08:16 +0100

    Add script to trigger on specific batches

---
+++ src/main/scripts/trigger-on-specific-batch.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Initiate creation of a specific batch roundtrip in DOMS
+#
+# Parameters:
+# $1 : path to config file
+# $2 : batch id
+# $3 : batch roundtrip number
+#
+# Author: jrg, ktc, kfc
+#
+
+# Use null-globbing so * can expand to empty and we can avoid nasty surprises
+shopt -s nullglob
+
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+
+trigger_file=transfer_acknowledged
+
+config="$1"
+if [ -z "$config" ]; then
+	echo "config not received" >&2
+	echo "usage: $(basename $0) /path/to/config.sh" >&2
+	exit 1
+fi
+
+source "$config"
+
+# Check that mandatory settings are atleast defined
+for var in path_to_dir_of_batches donedir trigger_name url_to_doms doms_username doms_password url_to_pid_gen
+do
+    [ -z "${!var}" ] && echo "ERROR: $config must define \$$var" && exit 2
+done
+
+cd "$path_to_dir_of_batches"
+
+batch_id=$2
+roundtrip=$3
+batch_dirname="B${batch_id}-RT${roundtrip}"
+
+# Check for trigger-file
+if [ ! -f "$batch_dirname/$trigger_file" ]; then
+	# Trigger-file does not exist, so batch is not ready for us, skip it
+	echo "This batch is not acknowledged. Exiting."
+	exit 1
+fi
+
+# Skip batches that are already done
+if [ -f "$donedir/$batch_dirname" ]; then
+	echo "This batch is already triggered. Exiting."
+	exit 1
+fi
+
+# Mark batch as done, by creating an empty file with the batch's name
+touch "$donedir/$batch_dirname"
+
+# Create batch in DOMS
+java -classpath $SCRIPT_PATH/../conf/:$SCRIPT_PATH/../lib/'*' dk.statsbiblioteket.medieplatform.autonomous.newspaper.CreateBatch "$batch_id" "$roundtrip" "$trigger_name" "$url_to_doms" "$doms_username" "$doms_password" "$url_to_pid_gen"
+
